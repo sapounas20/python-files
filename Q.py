6037c26d7f7222b43a391d465c400ec04e399245
Antonis Sapounas <antonis.sapounas@gmail.com>
2023-12-17 01:28:27 +0200

    create new Q3 file and fix Q.py

--- Q.py
+++ Q.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-
-K = 1e-15  # Arbitrary constant
-braking_index = 3  # Braking index
+I1 = 1.4* 1.9885 * 10**30 * 10**8 * 0.4
+I2 = 1.5 *10**45 / 1000 /10000
+braking_index = 5  # Braking index
 
 
 Q = np.array([0.9, 1.6, 1.5, 1.2, 1.2, 1.5, 1, 1.8, 1.2, 2.7, 0.5])
 tau_c = np.array([4.93, 11.3, 43.6, 12.1, 20.6, 15.5, 58.4, 15.8, 21.5, 38.8, 10.5]) #kyr
 
 t= np.arange(0, 70, 1)
-glitch_time = len(t)//2
+glitch_time = len(t)//3
 
 tau= []
 for i in range(len(Q)):
@@ -23,17 +24,19 @@ for j in range(len(tau)):
     values= []
     for k in range(len(t)):
         D= 0
-        if k >= glitch_time:
-            D += values[k-1] * 1.5 - values[k-1]
+        if k <= glitch_time:
+            omega = 1000* 2* np.pi- braking_index * 24* 3600* t[k]/ 2000
+        else:
+            D += omega * 1.25 - omega
         
-        omega_0 = 1000*2* np.pi - braking_index * 24* 3600* t[k]/ (5000)
-        omega= omega_0 + D * (Q[j]/100 *np.exp(-(24* 3600* t[k])/(10**3 * 365* 24* 3600* tau[j]))+1-Q[j]/100)
+            omega_0 = 1000* 2* np.pi- braking_index * 24* 3600* t[k]/ 5000
+            omega= omega_0 + D * (Q[j]/100 *math.exp(-( 24* 3600* t[k])/( 3*365 *24* 3600* tau[j]))+1- Q[j]/100)
 
         values.append(omega)
     
     O.append(values)
 
-print(O)
+
 
 for j in range(len(O)):
     plt.plot(t, O[j], label=f'Set {j + 1}')
